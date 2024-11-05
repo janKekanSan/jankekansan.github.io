dd9da5cb394b83f04fbadc617d9f0b5661157003
Gregory Danielson <gregory.danielson3@gmail.com>
2024-11-05 08:36:42 -0600

    o pana e sona ni tawa ilo pi kalama lukin: nena Toggle li pali ala pali

--- static/sptoggle.js
+++ static/sptoggle.js
@@ -1,4 +1,6 @@
 function toggle() {
+  var button = document.getElementById("sptoggle");
+
   var stylesheet = document.styleSheets[0];
   var elementRules;
   for (var i = 0; i < stylesheet.cssRules.length; i++) {
@@ -10,7 +12,9 @@ function toggle() {
   var current = elementRules.style.getPropertyValue("font-family");
   if (current !== "sp") {
     elementRules.style["font-family"] = "sp";
+    button.ariaPressed = "false";
   } else {
     elementRules.style["font-family"] = "";
+    button.ariaPressed = "true";
   }
 }
